3b90f12b1058286b7d54756479110e85be2754a2
Estela Franco <estela.francoc@gmail.com>
2021-02-08 17:30:34 +0100

    Create test-lighthouse.sh

---
+++ test-lighthouse.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mkdir "$(date +%F)"
+cat urls.txt | while read line 
+do
+    name=$(echo "$line" | sed 's/https:\/\///g' | sed 's/\//_/g' | sed 's/\./_/g')
+    lighthouse $line --output json > "$(date +%F)"/"$line".json
+done
