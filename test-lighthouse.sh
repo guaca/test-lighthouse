131ba0e7a4aa922e22beb46c582ef673190e8767
Estela Franco <estela.francoc@gmail.com>
2021-02-08 18:34:33 +0100

    Update test-lighthouse.sh
    Fix file name definition

--- test-lighthouse.sh
+++ test-lighthouse.sh
@@ -3,5 +3,5 @@ mkdir "$(date +%F)"
 cat urls.txt | while read line 
 do
     name=$(echo "$line" | sed 's/https:\/\///g' | sed 's/\//_/g' | sed 's/\./_/g')
-    lighthouse $line --output json > "$(date +%F)"/"$line".json
+    lighthouse $line --output json > "$(date +%F)"/"$name".json
 done
